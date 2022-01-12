00da998d4d1d1384a54f2f857c3c4397d0eb2cf5
Victor Caldeira <victorhenriquebarbosa@gmail.com>
2022-01-12 10:56:44 -0400

    Add telegram bot and other stuff

--- config.py
+++ config.py
@@ -4,8 +4,13 @@ import os
 api_key = os.environ.get('api_key')
 api_secret = os.environ.get('api_secret')
 
+# Telegram chat bot token
+BOT_TOKEN = os.environ.get('bot_token')
+CHAT_ID = os.environ.get('chat_id')
+
 # Strategy config
 asset = 'ETHUSDT'
+first_trade_value = 15 #USDT
 window = 11
 trigger = 0.005
 trade_delay = 7200 # minimum delay in seconds between 2 trades
--- print_to_txt.py
+++ print_to_txt.py
@@ -1,6 +1,7 @@
 import sys
+import requests
 
-def print_to_txt(message, file):
+def print_to_txt(message, file, BOT_TOKEN, CHAT_ID):
     print(message)
 
     original_stdout = sys.stdout # Save a reference to the original standard output
@@ -11,3 +12,8 @@ def print_to_txt(message, file):
         sys.stdout = original_stdout # Reset the standard output to its original value
     
     f.close()
+
+    # Send message to telegram
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    params = { "chat_id": CHAT_ID, "text": message }
+    requests.get(url, params=params)
