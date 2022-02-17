36556278523ab5e31317d12220d361cbb0375fb6
Victor Caldeira <victorhenriquebarbosa@gmail.com>
2022-02-16 21:35:17 -0400

    Changes: Catch exceptions on API requests, configs as environment variables, add logServices.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ BOT_TOKEN = os.environ.get('bot_token')
 CHAT_ID = os.environ.get('chat_id')
 
 # Strategy config
-asset = 'ETHUSDT'
-first_trade_value = 15 #USDT
-window = 11
-trigger = 0.005
-trade_delay = 7200 # minimum delay in seconds between 2 trades
+asset = os.environ.get('asset') #'ETHUSDT'
+first_trade_value = os.environ.get('first_trade_value') # 12 #USDT
+window = os.environ.get('window') #11
+trigger = os.environ.get('trigger') #0.005
+trade_delay = os.environ.get('trade_delay') #7200 # minimum delay in seconds between 2 trades
